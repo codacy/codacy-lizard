ad06f5094ebb308b826967fecbb36dde0aebe9a3
Alejandro Rizzo <alerizzo@gmail.com>
2024-05-23 17:46:40 +0100

    remove comments

--- src/configCreator.ts
+++ src/configCreator.ts
@@ -1,17 +1,6 @@
-import { Codacyrc, Parameter, ParameterSpec, Pattern } from "codacy-seed"
+import { Codacyrc } from "codacy-seed"
 import { debug } from "./logging"
 import { toolName } from "./toolMetadata"
-// import { ESLint, Linter } from "eslint"
-// import { existsSync } from "fs-extra"
-// import { cloneDeep, fromPairs, isEmpty, partition } from "lodash"
-// import path from "path"
-
-// import { isBlacklisted } from "./blacklist"
-// import { DocGenerator } from "./docGenerator"
-// import { defaultOptions } from "./eslintDefaultOptions"
-// import { getAllRules, getPluginsName } from "./eslintPlugins"
-// import { DEBUG, debug } from "./logging"
-// import { patternIdToEslint } from "./model/patterns"
 
 export interface LizardOptions {
   files: string[]
@@ -20,7 +9,6 @@ export interface LizardOptions {
 }
 
 export const getLizardOptions = async function (
-  // srcDirPath: string,
   codacyrc: Codacyrc,
 ): Promise<LizardOptions> {
   debug("config: creating")
